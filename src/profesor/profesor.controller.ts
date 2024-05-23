fcf7cc320d31f231e38e81f2457c8f4a64f28267
CristianCaro915 <c.caro@uniandes.edu.co>
2024-05-23 10:47:19 -0500

    new changes

--- src/profesor/profesor.controller.ts
+++ src/profesor/profesor.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Body, Controller, Delete, Get, HttpCode, Param, Post, Put, UseInterceptors } from '@nestjs/common';
+import { plainToInstance } from 'class-transformer';
+import { BusinessErrorsInterceptor } from '../shared/interceptors/business-errors/business-errors.interceptor';
+import { ProfesorEntity } from './profesor.entity/profesor.entity';
+import { ProfesorService } from './profesor.service';
+
 
 @Controller('profesor')
-export class ProfesorController {}
+@UseInterceptors(BusinessErrorsInterceptor)
+export class EstudianteController {
+    constructor(private readonly profesorService: ProfesorService){}
+
+    @Get("profesor:id")
+    async findById(@Param('estudianteId') id: string){
+        return await this.profesorService.findOne(id);
+    }
+}
